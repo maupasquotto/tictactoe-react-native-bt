d95cd565de860d43cb188309ac34bdf31d7fd0cd
Mauricio Pasquotto <maupasquotto@gmail.com>
2018-05-19 21:49:28 -0300

    V1 - Offline mode

--- app/lib/TicTacToe.js
+++ app/lib/TicTacToe.js
@@ -11,53 +11,127 @@ import {Button, StyleSheet, Text, TouchableHighlight, View, ToastAndroid} from '
  */
 export default class TicTacToe extends React.Component
 {
+    /**
+     * Game states that control entire app
+     *
+     * @author Mauricio de Castro Pasquotto
+     * @date 19/05/2018
+     * @type {{gameBoard: *[], nextMove: string, gameEnded: boolean}}
+     */
     state = {
         gameBoard: [
             ['', '', ''],
             ['', '', ''],
             ['', '', '']
         ],
-        nextMove: 'X'
+        nextMove: 'X',
+        gameEnded: false
     };
 
-    constructor(props)
+
+    /**
+     * The constructor calls super constructor and resets the game automatically
+     *
+     * @author Mauricio de Castro Pasquotto
+     * @date 19/05/2018
+     */
+    constructor()
     {
         /* Call super class and reset game */
-        super(props);
+        super();
         this.resetGame();
     }
 
+    /**
+     * Reset game and all states
+     *
+     * @author Mauricio de Castro Pasquotto
+     * @date 19/05/2018
+     */
     resetGame()
     {
         /* Reset Virtual Board */
-        this.state.gameBoard = [
-            ['', '', ''],
-            ['', '', ''],
-            ['', '', '']
-        ];
+        this.setState({
+            gameBoard : [
+                ['', '', ''],
+                ['', '', ''],
+                ['', '', '']
+            ],
+            nextMove: 'X',
+            gameEnded: false
+        });
     }
 
-    /* Check if the game is done and return who won */
-    gameEnd()
+    /**
+     * Verify whether the game is over or not
+     *
+     * @author Mauricio de Castro Pasquotto
+     * @date 19/05/2018
+     * @returns {*}
+     */
+    gameEnded()
     {
-        return false;
+        let gameBoard = this.state.gameBoard;
+
+        /* Check rows and columns */
+        for (let i = 0; i < 3; i++) {
+            /* Check rows */
+            if (gameBoard[i][0] === gameBoard[i][1] && gameBoard[i][0] === gameBoard[i][2] && ['X', 'O'].indexOf(gameBoard[i][0]) >= 0) {
+                return gameBoard[i][0];
+            }
+
+            /* Check columns */
+            if (gameBoard[0][i] === gameBoard[1][i] && gameBoard[0][i] === gameBoard[2][i] && ['X', 'O'].indexOf(gameBoard[0][i]) >= 0) {
+                return gameBoard[0][i];
+            }
+        }
+
+        /* Check Diagonals */
+        if (((gameBoard[2][0] === gameBoard[1][1] && gameBoard[2][0] === gameBoard[0][2]) || // Diagonal Left Top -> Bottom right (\)
+            (gameBoard[0][0] === gameBoard[1][1] && gameBoard[0][0] === gameBoard[2][2])) && // Diagonal Right Top -> Bottom left (/)
+            ['X', 'O'].indexOf(gameBoard[1][1]) >= 0) {
+            return gameBoard[1][1];
+        }
+
+        /* Check whether nobody won */
+        for (let i = 0; i < 3; i++) {
+            for (let x = 0; x < 3; x++) {
+                if (gameBoard[0][0] === '') {
+                    return false;
+                }
+            }
+        }
+
+        return 'Nobody';
     }
 
+    /**
+     * Return next to move and set its state
+     *
+     * @author Mauricio de Castro Pasquotto
+     * @date 19/05/2018
+     * @returns {string}
+     */
     getNextMove()
     {
         let v = this.state.nextMove;
 
         if (v === 'X') {
             this.setState({nextMove: 'O'});
-            //this.state.nextMove = 'O';
         } else {
             this.setState({nextMove: 'X'});
-            //this.state.nextMove = 'X';
         }
 
         return v;
     }
 
+    /**
+     * Set successful movement
+     *
+     * @param x
+     * @param y
+     * @param t
+     */
     setMovement(x, y, t) {
         let gameBoard = this.state.gameBoard;
         gameBoard[x][y] = t;
@@ -68,83 +142,114 @@ export default class TicTacToe extends React.Component
         });
     }
 
+    /**
+     * Execute movement
+     *
+     * @author Mauricio de Castro Pasquotto
+     * @date 19/05/2018
+     * @param x
+     * @param y
+     * @returns {boolean}
+     */
     doMovement(x, y)
     {
 
-        if (this.gameEnd()) {
-            this.showWarning('Game is over!');
+        /* Set movement */
+        if (this.state.gameBoard[x][y] === '' && !this.state.gameEnded) {
+
+            this.setMovement(x, y, this.getNextMove());
         }
 
-        if (this.state.gameBoard[x][y] === '') {
-            this.setMovement(x, y, this.getNextMove());
-            // this.state.gameBoard[x][y] = this.getNextMove();
-        } else {
-            this.showWarning('Space already occupied!');
+        /* Define whether the game is over or not */
+        let gameEnded = this.gameEnded();
+        if (gameEnded !== false) {
+            this.setState({gameEnded: true});
+            TicTacToe.showWarning('Game is over! ' + gameEnded + ' won!');
+            return false;
         }
     }
 
-    showWarning(msg)
+    /**
+     * Show a toast message
+     *
+     * @author Mauricio de Castro Pasquotto
+     * @date 19/05/2018
+     * @param msg
+     */
+    static showWarning(msg)
     {
         ToastAndroid.show(msg, ToastAndroid.SHORT);
     }
 
+    /**
+     * Return the game board so you can play !
+     *
+     * @author Mauricio de Castro Pasquotto
+     * @date 19/05/2018
+     * @return {*}
+     */
     render()
     {
         return (
-            <View style={styles.mainWrapper}>
-                {/* Game row 1 */}
-                <View style={styles.row}>
-                    <TouchableHighlight onPress={() => {this.doMovement(0, 0)}}>
-                        <View style={[styles.gridSquare, styles.borderRight, styles.borderBottom]}>
-                            <Text style={styles.txtStyle}>{this.state.gameBoard[0][0]}</Text>
-                        </View>
-                    </TouchableHighlight>
-                    <TouchableHighlight onPress={() => {this.doMovement(0, 1)}}>
-                        <View style={[styles.gridSquare, styles.borderRight, styles.borderBottom]}>
-                            <Text style={styles.txtStyle}>{this.state.gameBoard[0][1]}</Text>
-                        </View>
-                    </TouchableHighlight>
-                    <TouchableHighlight onPress={() => {this.doMovement(0, 2)}}>
-                        <View style={[styles.gridSquare, styles.borderBottom]}>
-                            <Text style={styles.txtStyle}>{this.state.gameBoard[0][2]}</Text>
-                        </View>
-                    </TouchableHighlight>
+            <View style={{flex: 1}}>
+                <View style={styles.mainWrapper}>
+                    {/* Game row 1 */}
+                    <View style={styles.row}>
+                        <TouchableHighlight onPress={() => {this.doMovement(0, 0)}}>
+                            <View style={[styles.gridSquare, styles.borderRight, styles.borderBottom]}>
+                                <Text style={styles.txtStyle}>{this.state.gameBoard[0][0]}</Text>
+                            </View>
+                        </TouchableHighlight>
+                        <TouchableHighlight onPress={() => {this.doMovement(0, 1)}}>
+                            <View style={[styles.gridSquare, styles.borderRight, styles.borderBottom]}>
+                                <Text style={styles.txtStyle}>{this.state.gameBoard[0][1]}</Text>
+                            </View>
+                        </TouchableHighlight>
+                        <TouchableHighlight onPress={() => {this.doMovement(0, 2)}}>
+                            <View style={[styles.gridSquare, styles.borderBottom]}>
+                                <Text style={styles.txtStyle}>{this.state.gameBoard[0][2]}</Text>
+                            </View>
+                        </TouchableHighlight>
+                    </View>
+                    {/* Game row 2 */}
+                    <View style={styles.row}>
+                        <TouchableHighlight onPress={() => {this.doMovement(1, 0)}}>
+                            <View style={[styles.gridSquare, styles.borderRight, styles.borderBottom]}>
+                                <Text style={styles.txtStyle}>{this.state.gameBoard[1][0]}</Text>
+                            </View>
+                        </TouchableHighlight>
+                        <TouchableHighlight onPress={() => {this.doMovement(1, 1)}}>
+                            <View style={[styles.gridSquare, styles.borderRight, styles.borderBottom]}>
+                                <Text style={styles.txtStyle}>{this.state.gameBoard[1][1]}</Text>
+                            </View>
+                        </TouchableHighlight>
+                        <TouchableHighlight onPress={() => {this.doMovement(1, 2)}}>
+                            <View style={[styles.gridSquare, styles.borderBottom]}>
+                                <Text style={styles.txtStyle}>{this.state.gameBoard[1][2]}</Text>
+                            </View>
+                        </TouchableHighlight>
+                    </View>
+                    {/* Game row 3 */}
+                    <View style={styles.row}>
+                        <TouchableHighlight onPress={() => {this.doMovement(2, 0)}}>
+                            <View style={[styles.gridSquare, styles.borderRight]}>
+                                <Text style={styles.txtStyle}>{this.state.gameBoard[2][0]}</Text>
+                            </View>
+                        </TouchableHighlight>
+                        <TouchableHighlight onPress={() => {this.doMovement(2, 1)}}>
+                            <View style={[styles.gridSquare, styles.borderRight]}>
+                                <Text style={styles.txtStyle}>{this.state.gameBoard[2][1]}</Text>
+                            </View>
+                        </TouchableHighlight>
+                        <TouchableHighlight onPress={() => {this.doMovement(2, 2)}}>
+                            <View style={[styles.gridSquare]}>
+                                <Text style={styles.txtStyle}>{this.state.gameBoard[2][2]}</Text>
+                            </View>
+                        </TouchableHighlight>
+                    </View>
                 </View>
-                {/* Game row 2 */}
-                <View style={styles.row}>
-                    <TouchableHighlight onPress={() => {this.doMovement(1, 0)}}>
-                        <View style={[styles.gridSquare, styles.borderRight, styles.borderBottom]}>
-                            <Text style={styles.txtStyle}>{this.state.gameBoard[1][0]}</Text>
-                        </View>
-                    </TouchableHighlight>
-                    <TouchableHighlight onPress={() => {this.doMovement(1, 1)}}>
-                        <View style={[styles.gridSquare, styles.borderRight, styles.borderBottom]}>
-                            <Text style={styles.txtStyle}>{this.state.gameBoard[1][1]}</Text>
-                        </View>
-                    </TouchableHighlight>
-                    <TouchableHighlight onPress={() => {this.doMovement(1, 2)}}>
-                        <View style={[styles.gridSquare, styles.borderBottom]}>
-                            <Text style={styles.txtStyle}>{this.state.gameBoard[1][2]}</Text>
-                        </View>
-                    </TouchableHighlight>
-                </View>
-                {/* Game row 3 */}
-                <View style={styles.row}>
-                    <TouchableHighlight onPress={() => {this.doMovement(2, 0)}}>
-                        <View style={[styles.gridSquare, styles.borderRight]}>
-                            <Text style={styles.txtStyle}>{this.state.gameBoard[2][0]}</Text>
-                        </View>
-                    </TouchableHighlight>
-                    <TouchableHighlight onPress={() => {this.doMovement(2, 1)}}>
-                        <View style={[styles.gridSquare, styles.borderRight]}>
-                            <Text style={styles.txtStyle}>{this.state.gameBoard[2][1]}</Text>
-                        </View>
-                    </TouchableHighlight>
-                    <TouchableHighlight onPress={() => {this.doMovement(2, 2)}}>
-                        <View style={[styles.gridSquare]}>
-                            <Text style={styles.txtStyle}>{this.state.gameBoard[2][2]}</Text>
-                        </View>
-                    </TouchableHighlight>
+                <View style={{flex: 1}}>
+                    <Button title="Reset" onPress={() => {this.resetGame()}} />
                 </View>
             </View>
         );
@@ -152,6 +257,13 @@ export default class TicTacToe extends React.Component
 
 }
 
+/**
+ * Style !
+ *
+ * @author Mauricio de Castro Pasquotto
+ * @date 19/05/2018
+ * @type {*|{panHandlers, getInteractionHandle}|{duration, create, update, delete}|{type, property}}
+ */
 const styles = StyleSheet.create({
     gridSquare: {
         height: 70,
@@ -176,7 +288,7 @@ const styles = StyleSheet.create({
     },
 
     mainWrapper: {
-        flex: 1,
+        flex: 3,
         flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center'
